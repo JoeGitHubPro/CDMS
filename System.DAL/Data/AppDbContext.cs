e35e665f491d35d373477262bd3fe3cd34b09bc5
Youssef Mohamed Ali <95888292+JoeGitHubPro@users.noreply.github.com>
2024-07-18 19:26:21 +0300

    Initial Data Access Layer

---
+++ System.DAL/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System.DAL.Models;
+
+namespace System.DAL.Data
+{
+    public class AppDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
